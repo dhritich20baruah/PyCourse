765b27641406ccbd18a3ffa06fba6488204d998f
Delta <dhriti.baruah20@gmail.com>
2022-01-06 12:18:04 +0530

    while-loop

--- user_input_and_while_loop.py
+++ user_input_and_while_loop.py
@@ -28,10 +28,101 @@
 #     print('\nYou are not tall enough')
 
 # The Modulo Operator
-number = input("Enter a number, and I'll tell you if it's even or odd: ")
-number = int(number)
+# number = input("Enter a number, and I'll tell you if it's even or odd: ")
+# number = int(number)
 
-if number % 2 == 0:
-    print(f"\nThe number {number} is even")
-else:
-    print(f"\nThe Number {number} is odd")
+# if number % 2 == 0:
+#     print(f"\nThe number {number} is even")
+# else:
+#     print(f"\nThe Number {number} is odd")
+
+
+# While Loop
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
+
+# Letting the User choose when to quit
+# prompt = "\nTell me something, and I will repeat it back to you: "
+# prompt += "\nEnter 'quit' to end the program."
+# message = ""
+# while message != 'quit':
+#     message = input(prompt)
+
+#     if message != 'quit':
+#         print(message)
+
+#################
+# Flag
+# prompt = "\nTell me something, and I will repeat it back to you: "
+# prompt += "\nEnter 'quit' to end the program."
+# active = True
+# while active:
+#     message = input(prompt)
+
+#     if message == 'quit':
+#         active = False
+#     else:
+#         print(message)
+
+###############
+#Using break to exit a loop
+# prompt = "\nPlease enter the name of a city you have visited: "
+# prompt += "\n(Enter 'quit' when you are finished.)"
+# while True: 
+#     city = input(prompt)
+#     if city == 'quit':
+#         break
+#     else:
+#         print(f"I'd love to go to {city.title()}")
+
+######################
+#Using continue in a loop
+current_number = 0
+while current_number < 10:
+    current_number += 1
+    if current_number % 2 == 0:
+        continue
+    print(current_number)
+
+# Using a while loop with list and dictionaries
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+
+    print(f"Verifying user: {current_user.title()}")
+    confirmed_users.append(current_user)
+
+print('\nThe following users have been confirmed:')
+for confirmed_user in confirmed_users:
+    print(confirmed_user.title())
+
+# Remove all instances of a specific values from a list
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+
+while 'cat' in pets:
+    pets.remove('cat')
+
+print(pets)
+
+# Filling a Dictionary with User Input
+responses = {}
+#set a flas to indicate that apolling is active
+polling_active = True
+while polling_active:
+    name = input("\nWhat is your name?")
+    response = input("What will you study today?")
+    # Store the responses in the dictionary.
+    responses[name] = response
+    # Find out if anyone else is going to take the poll
+    repeat = input("Would you like to let another person repond? (yes/no)")
+    if repeat == 'no':
+        polling_active = False
+
+# Polling is complete. Show the results.
+print("\n---Poll Results---")
+for name, response in responses.items():
+    print(f"{name} would like to study {response}.")
