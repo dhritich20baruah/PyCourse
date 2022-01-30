a975534bdc3eebf675e22e91ec2ca5cda82c78ae
Delta <dhriti.baruah20@gmail.com>
2022-01-30 21:34:27 +0530

    DSA added

--- functions.py
+++ functions.py
@@ -39,6 +39,63 @@ describe_pet(pet_name='Tommy')
 describe_pet('Harry', 'hamster')
 describe_pet(pet_name="Harry", animal_type='hamster')
 
+# Return values
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted"""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
 
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
 
+# Make arguments optional
+def get_formatted_name(first_name, last_name,middle_name=''):
+    """Return a full name, neatly formatted"""
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name = f"{first_name} {last_name}"
+    return full_name.title()
 
+musician = get_formatted_name('jimi', 'hendrix')
+musician = get_formatted_name('Sir', 'Elvis', 'Presly')
+print(musician)
+
+# Returning a Dictionary
+def build_person(first_name, last_name):
+    """Return a dictonary of information about a person"""
+    person = {'first': first_name, 'last': last_name}
+    return person
+
+musician = build_person('jimi', 'hendrix')
+print(musician)
+
+def build_person(first_name, last_name, age=None):
+    """Return a dictonary of information about a person"""
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
+
+musician = build_person('jimi', 'hendrix', age=30)
+print(musician)
+
+# Using a function with a while loop
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted"""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+while True:
+    print('\nPlease tell me your name:')
+    print("(enter 'q' at any time to quit)")
+
+    f_name = input("First name: ")
+    if f_name == 'q':
+        break
+    l_name = input("Last name: ")
+    if l_name == 'q':
+        break
+
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}")
