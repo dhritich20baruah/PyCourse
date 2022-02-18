08d81c8ad331f1e79d198d8cc6877abec4d74456
Delta <dhriti.baruah20@gmail.com>
2022-02-18 23:29:54 +0530

    ex1

---
+++ ex1.py
@@ -0,0 +1,15 @@
+words = {
+    'clock': 'device that gives time',
+    'linked': 'connected',
+    'mask': 'covering a thing',
+    'switch': 'change',
+    'perfume': 'substance that smells good'
+}
+
+message = input("Tell me what do you want to know about: ")
+
+prompt = input(message)
+
+for word, meaning in words:
+    if prompt == word:
+        print(meaning)
