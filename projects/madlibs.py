c846176e2d8ca5c210cb6dff74e6addd3a4e71d8
Delta <dhriti.baruah20@gmail.com>
2022-07-17 21:06:59 +0530

    Projects started

---
+++ projects/madlibs.py
@@ -0,0 +1,11 @@
+# youtuber= "Delta"
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+madlib = f"Computer progeaming is so {adj}! It makes me so excited all the time./ I love to {verb1}. Stay hydrated and {verb2} like you are a {famous_person}! "
+print(madlib)
